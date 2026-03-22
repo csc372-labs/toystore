dbf14a4fdb359165328026ea8ad18576bf599342
Liam McKenzie <157838863+lmckenzie99@users.noreply.github.com>
2026-03-22 10:02:30 -0400

    Update database connection details

--- database-connection.php
+++ database-connection.php
@@ -1,13 +1,13 @@
 <?php                                             // Opening PHP tag
 
 $type     = 'mysql';                             // Type of database
-$server   = '127.0.0.1';                    // Server the database is on
+$server   = '192.185.94.204';                    // Server the database is on
 $db       = 'liammcke_toystore';     // Name of the database
 $port     = '3306';                           // Port is usually 3306 in Hostgator
 $charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
 
-$username = 'liammcke_tsUser';     // Enter YOUR cPanel username and user here
-$password = 'PHPIsCool!';           // Enter YOUR user password here
+$username = 'liammckenzie';     // Enter YOUR cPanel username and user here
+$password = 'oZc_b3vPZCfDeJubTnU';           // Enter YOUR user password here
 
 
 
